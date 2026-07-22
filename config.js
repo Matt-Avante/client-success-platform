e59660f16315c1a411517e003f90ef11f196a10e
Matt-Avante <matt@avantemedia.co>
2026-07-22 09:38:46 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.CONFIG = {
-    responseEndpoint: "https://hook.eu2.make.com/1mjmam4ck1mpikfp12g8cgq29hdrx5sv",
-    accountManager: "Matthew",
-    downloadBackup: true
+  responseEndpoint: "https://hook.eu2.make.com/1mjmam4ck1mpikfp12g8cgq29hdrx5sv",
+  accountManager: "Matthew",
+  downloadBackup: true
 };
